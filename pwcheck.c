e985ae6cdfaf7b0ed5543d1224f618c775428eec
Marek Prochazka <m.prochazka2002@gmail.com>
2021-10-17 14:44:45 +0200

    without stats and too long passwd

--- pwcheck.c
+++ pwcheck.c
@@ -4,8 +4,10 @@
 void remove_break(char string[100]);
 int str_len(const char string[]);
 int str_same(const char first_string[], const char second_string[]);
+int str_isnum(const char string[]);
 const char *str_strip(const char string[], int start, int end);
 int *get_params(int argc, char const *argv[]);
+int validate_input(const int LEVEL,const int PARAM, int argc, char const *argv[]);
 int check_level1(const char string[]);
 int check_level2(const char string[], const int PARAM);
 int check_level3(const char string[], const int PARAM);
@@ -14,13 +16,16 @@ void process_checks(const int LEVEL, const int PARAM, const int STATS);
 
 int main(int argc, char const *argv[])
 {
-
     int *parametrs = get_params(argc, argv);
     const int LEVEL = *parametrs;
     const int PARAM = *(parametrs + 1);
     const int STATS = *(parametrs + 2);
-    process_checks(LEVEL, PARAM, STATS);
-    return 0;
+    if (validate_input(LEVEL,PARAM, argc, argv) == 1)
+    {
+        process_checks(LEVEL, PARAM, STATS);
+        return 0;
+    }
+    return 1;
 }
 
 void remove_break(char string[100])
@@ -42,18 +47,43 @@ int str_len(const char string[])
     return count;
 }
 
-int str_same(const char first_string[], const char second_string[])
+int str_same(const char *first_string, const char *second_string)
 {
     int are_same = 1;
-    for (int i = 0; first_string[i] != '\0' && second_string[i] != '\0'; i++)
+
+    // Iterate a loop till the end
+    // of both the strings
+    while (*first_string != '\0' || *second_string != '\0')
     {
-        if (first_string[i] != second_string[i])
+        if (*first_string == *second_string)
+        {
+            first_string++;
+            second_string++;
+        }
+
+        // If two characters are not same
+        // print the difference and exit
+        else if ((*first_string == '\0' && *second_string != '\0') || (*first_string != '\0' && *second_string == '\0') || *first_string != *second_string)
         {
             are_same = 0;
             break;
         }
     }
+
+    // If two strings are exactly same
     return are_same;
+}
+int str_isnum(const char string[])
+{
+    int is_num = 1;
+    for (int i = 0; i < str_len(string); i++)
+    {
+        if (string[i] < '0' || string[i] > '9')
+        {
+            is_num = 0;
+        }
+    }
+    return is_num;
 }
 
 int *get_params(int argc, char const *argv[])
@@ -92,6 +122,54 @@ int *get_params(int argc, char const *argv[])
     return output;
 }
 
+int validate_input(const int LEVEL,const int PARAM, int argc, char const *argv[])
+{
+    int validated = 1;
+    int num_arg_count = 0;
+    if (LEVEL > 4 || LEVEL < 1)
+    {
+        validated = 0;
+        fprintf(stderr, "Invalid LEVEL param '%d'\n", LEVEL);
+    }
+        if (PARAM < 1)
+    {
+        validated = 0;
+        fprintf(stderr, "Invalid PARAM param '%d'\n", PARAM);
+    }
+    if (argc > 6)
+    {
+        validated = 0;
+        fprintf(stderr, "Too many params\n");
+    }
+    if (argc < 3)
+    {
+        validated = 0;
+        fprintf(stderr, "Not enough params\n");
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        if (str_isnum(argv[i]) == 1)
+        {
+            num_arg_count++;
+        }
+        if (((str_isnum(argv[i])) ||
+             (str_same(argv[i], "-l")) ||
+             (str_same(argv[i], "-p")) ||
+             (str_same(argv[i], "--stats"))) != 1)
+        {
+            validated = 0;
+            fprintf(stderr, "Invalid param: %s\n", argv[i]);
+        }
+    }
+    if (num_arg_count > 2 || num_arg_count < 2)
+    {
+        validated = 0;
+        fprintf(stderr, "Invalid count of numeric params: %d\n", num_arg_count);
+    }
+
+    return validated;
+}
+
 int check_level1(const char string[])
 {
     int has_small_char = 0;
@@ -125,28 +203,34 @@ int check_level2(const char string[], const int PARAM)
 
     for (int i = 0; i < str_len(string); i++)
     {
-        if (string[i] >= 'a' && string[i] <= 'z' && has_small_char == 0)
+        if (string[i] >= 'a' && string[i] <= 'z')
         {
-            count_groups++;
+            if (has_small_char != 1)
+            {
+                count_groups++;
+            }
             has_small_char = 1;
+            continue;
         }
-        if (string[i] >= 'A' && string[i] <= 'Z' && has_big_char == 0)
+        if (string[i] >= 'A' && string[i] <= 'Z')
         {
-            count_groups++;
+            if (has_big_char != 1)
+            {
+                count_groups++;
+            }
             has_big_char = 1;
+            continue;
         }
         if (string[i] >= '0' && string[i] <= '9' && has_number_char == 0)
         {
-            count_groups++;
+            if (has_number_char != 1)
+            {
+                count_groups++;
+            }
             has_number_char = 1;
+            continue;
         }
-        if (
-            (
-                (string[i] >= 32 && string[i] <= 57) ||
-                (string[i] >= 72 && string[i] <= 100) ||
-                (string[i] >= 91 && string[i] <= 96) ||
-                (string[i] >= 123 && string[i] <= 126)) &&
-            has_special_char == 0)
+        if ((string[i] >= 32 && string[i] <= 126) == 1 && has_special_char == 0)
         {
             count_groups++;
             has_special_char = 1;
@@ -186,22 +270,22 @@ int check_level4(const char string[], const int PARAM)
     int is_ok = 1;
     for (int i = 0; i < str_len(string); i++)
     {
+
         // construct substring
         for (int j = 0; j < PARAM; j++)
         {
             substring[j] = string[i + j];
         }
-        for (int k = 1; k < str_len(string); k++)
+        for (int k = i + 1; k < str_len(string); k++)
         {
+
             // construct comparison
             for (int l = 0; l < PARAM; l++)
             {
-                comparison_string[l] = string[i + k + l];
+                comparison_string[l] = string[k + l];
             }
-            // printf("%s %s \n", substring, comparison_string);
             if (str_same(substring, comparison_string) == 1)
             {
-                printf("%s %s \n", substring, comparison_string);
                 is_ok = 0;
                 break;
             }
@@ -254,5 +338,4 @@ void process_checks(const int LEVEL, const int PARAM, const int STATS)
         }
         count++;
     }
-    printf("%d", count);
 }
